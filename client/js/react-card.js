f695ba61aeb03c04e0119da87906d200a2f089b0
nigel <incitist@hotmail.com>
2015-04-22 03:13:11 +0800

    test

--- client/js/react-card.js
+++ client/js/react-card.js
@@ -1394,11 +1394,11 @@ EventContact = React.createClass({
 		<div className="contact-footer">
 			<div className="contact">
 					<i className="icon-width organizer-icon mdi-social-person"></i>
-					<span className="contact-text"> {this.props.organizer} </span>
+					<div className="contact-text">{this.props.organizer}</div>
 			</div>
 			<div className="contact">
 					<i className="icon-width contact-icon mdi-communication-email"></i>
-					<span className="contact-text" dangerouslySetInnerHTML={{__html: rawMarkup}} />
+					<div className="contact-text" dangerouslySetInnerHTML={{__html: rawMarkup}} ></div>
 			</div>
 		</div>
 		);

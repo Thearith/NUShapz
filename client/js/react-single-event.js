23e160cb6a4699584dfe6bd8577bc8311cef2091
thearith <sothearith.sreang@gmail.com>
2015-04-22 15:45:11 +0800

    fix single event positioning

--- client/js/react-single-event.js
+++ client/js/react-single-event.js
@@ -38,7 +38,7 @@ var EventContact;
 
 var SERVER_SHARE_SINGLE_EVENT = "http://hapz.nusmods.com/staging/event/?id="; 
 var SERVER_POST_EVENT = "http://hapz.nusmods.com/api.php";
-var SERVER_GET_SINGLE_EVENT = "http://hapz.nusmods.com/event/?id="; 
+var SERVER_GET_SINGLE_EVENT = "http://hapz.nusmods.com/api.php?cmd=singleEvent&eventid="; 
 var QUERY = "id";
 
 // constants
